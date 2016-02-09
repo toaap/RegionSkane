f5c689f31cf30e42c53fb214dce1a56a4a241854
toaap <mrtoap@hotmail.com>
2016-02-09 20:27:44 +0100

    Regionskåne first push

---
+++ RegionSkane/RegionView.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace RegionSkane
+{
+    partial class RegionView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Visa artiklar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 211);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(564, 212);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(188, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Avtals nummer:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(452, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Leverantörs nummer:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(59, 239);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(796, 301);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(764, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Visa artiklar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(764, 181);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Generera PDF";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // RegionView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 552);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "RegionView";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ RegionSkane/RegionView.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RegionSkane.Controller;
+
+namespace RegionSkane
+{
+    public partial class RegionView : Form
+    {
+        public RegionView()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private RegionController controller = new RegionController();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string artNr = textBox1.Text;
+            DataSet ds = controller.ReadExcelToDataSet(artNr);
+
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = ds; // dataset
+            dataGridView1.DataMember = "table1"; // table name you need to show
+
+            if (dataGridView1.DataSource != null)
+            {
+                button3.Enabled = true;
+                button4.Enabled = true;
+            }
+            /*
+            int n;
+            bool isNumeric = int.TryParse(artNr, out n);
+            if (isNumeric || artNr == null || artNr == String.Empty)
+            {
+                DataSet ds = controller.ReadExcelToDataSet(artNr);
+
+                dataGridView1.AutoGenerateColumns = true;
+                dataGridView1.DataSource = ds; // dataset
+                dataGridView1.DataMember = "Blad1$"; // table name you need to show
+
+                if (dataGridView1.DataSource != null)
+                {
+                    button3.Enabled = true;
+                    button4.Enabled = true;
+                }
+            }
+            else
+            {
+                textBox1.Text = "Endast siffror!";
+            }
+             * */ 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*
+            string companyName = textBox1.Text;
+            string text = textBox2.Text;
+
+            controller.printSamplePdf(companyName, text);
+             * */
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string companyName = textBox2.Text;
+            DataSet ds = (DataSet)dataGridView1.DataSource;
+            DataTable dt = controller.GetCompanyArt(companyName, ds);
+            dataGridView1.DataSource = dt;
+
+            button3.Enabled = false;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+
+            if(dataGridView1.DataSource.GetType().ToString().Equals("System.Data.DataSet"))
+            {
+                DataSet ds = (DataSet)dataGridView1.DataSource;
+                dt = ds.Tables["table1"];
+            }
+            else if (dataGridView1.DataSource.GetType().ToString().Equals("System.Data.DataTable"))
+            {
+                dt = (DataTable)dataGridView1.DataSource;
+            }
+            
+            
+            controller.Crud(dt);
+        }
+
+
+    }
+}
